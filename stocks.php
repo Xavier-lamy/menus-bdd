220a05f1fe52c238134517d5038e08afc14b642f
Xavier-lamy <xavier.lamy.pro@gmail.com>
2021-11-17 15:17:42 +0100

    Adding modify button to row in stocks

--- stocks.php
+++ stocks.php
@@ -7,18 +7,20 @@
     <div class="wrapper">
         <main class="element--center">
             <h1 class="text--center">Stocks</h1>
-            <table class="element--center table w--50">
+            <table class="element--center table--striped w--50">
                 <thead class="w--100 bg--secondary text--light">
-                    <th class="w--40">Ingredient</th>
-                    <th class="w--30">Quantity</th>
-                    <th>Use-by Date</th>
+                    <th class="w--25">Ingredient</th>
+                    <th class="w--25">Quantity</th>
+                    <th class="w--25">Use-by Date</th>
+                    <th class="w--25">Modifications</th>
                 </thead>
                 <tbody>
                     <?php foreach ($result as $product): ?>
                         <tr>
-                            <td class="text--center border--top"><? echo $product['ingredient']; ?></td>
-                            <td class="text--center"><? echo $product['quantity']. ' ' . $product['quantity_name']; ?></td>
-                            <td class="text--center"><? echo $product['useby_date']; ?></td>
+                            <td class="text--center p--1"><? echo $product['ingredient']; ?></td>
+                            <td class="text--center p--1"><? echo $product['quantity']. ' ' . $product['quantity_name']; ?></td>
+                            <td class="text--center p--1"><? echo $product['useby_date']; ?></td>
+                            <td class="text--center p--1"><a href="modify.php?id=<?php echo $product['id'] ?>" class="button--sm">Modify</a></td>
                         </tr>                        
                     <?php endforeach; ?>
                 </tbody>
