4479c137e96d7b3460df29753e0908f6c1d14846
Xavier-lamy <xavier.lamy.pro@gmail.com>
2021-11-18 11:29:55 +0100

    Separate connect and disconnect from _read

--- inc/_read.php
+++ inc/_read.php
@@ -1,6 +1,4 @@
 <?php
-//Connection
-require_once dirname(__DIR__)."/inc/_connect.php";
 
 //preparing query
 $query = $db->prepare($sql);
@@ -10,5 +8,3 @@ $query->execute();
 
 //store results in array
 $results = $query->fetchAll();
-
-require_once "./inc/_disconnect.php";
