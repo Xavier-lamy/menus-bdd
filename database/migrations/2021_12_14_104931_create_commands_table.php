2b4d40886adf40727b6a00e554675c53f0c8fda5
Xavierlamy <xavier.lamy.pro@gmail.com>
2021-12-14 12:02:46 +0100

    Create models and migrations for commands

---
+++ database/migrations/2021_12_14_104931_create_commands_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commands', function (Blueprint $table) {
+            $table->id('commands_id');
+            $table->string('ingredient', 60);
+            $table->unsignedInteger('quantity');
+            $table->string('quantity_name', 40);
+            $table->unsignedInteger('alert_stock');
+            $table->boolean('must_buy');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commands');
+    }
+}
