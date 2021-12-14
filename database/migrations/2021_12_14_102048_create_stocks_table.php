4b794609cad4ed959bd9cdd1427685352ff6f29f
Xavierlamy <xavier.lamy.pro@gmail.com>
2021-12-14 12:02:37 +0100

    Create models and migrations for stocks

---
+++ database/migrations/2021_12_14_102048_create_stocks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStocksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stocks', function (Blueprint $table) {
+            $table->id();
+            $table->string('ingredient', 60);
+            $table->unsignedInteger('quantity');
+            $table->string('quantity_name', 40);
+            $table->date('usedby_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stocks');
+    }
+}
