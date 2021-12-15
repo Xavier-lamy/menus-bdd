705935ec3d43f2d988d69a06c07eee51be92e492
Xavierlamy <xavier.lamy.pro@gmail.com>
2021-12-15 08:48:56 +0100

    Add one to many relation between command and stock

--- app/Models/Command.php
+++ app/Models/Command.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Stock;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Command extends Model
 {
     use HasFactory;
+
+    public function stocks(){
+        return $this->hasMany(Stock::class);
+    }
 }
--- app/Models/Stock.php
+++ app/Models/Stock.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Command;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
     use HasFactory;
+
+    public function command(){
+        return $this->belongsTo(Command::class);
+    }
 }
