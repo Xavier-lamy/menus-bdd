b6933325cd260548d4b80ea3cdf06dbbbe793b56
Xavierlamy <xavier.lamy.pro@gmail.com>
2022-01-14 14:52:26 +0100

    Add migrations and models for recipes

---
+++ app/Models/Recipe.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Quantity;
+
+class Recipe extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'process', 'total_weight'];
+
+    public $timestamps = false;
+
+    public function quantities(){
+        return $this->hasMany(Quantity::class);
+    }
+}
