3037b373fced4493c2690a980393010f9019c526
Xavierlamy <xavier.lamy.pro@gmail.com>
2022-01-14 14:53:27 +0100

    Add recipe controller with basics methods

---
+++ app/Http/Controllers/RecipeController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RecipeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('recipes');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $is_creating = true;
+
+        return view('recipe', [
+            'is_creating' => $is_creating,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return redirect('recipes')->with('success', 'Recipe created !');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('recipe');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $is_editing = true;
+        return view('recipe', [
+            'is_editing' => $is_editing,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        return redirect('recipe')->with('success', 'Recipe updated !');
+    }
+
+    /**
+     * Ask the user before removing.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function confirmDestroy(Request $request)
+    {
+        $delete_confirmation = 'recipes';
+        //...
+
+        return redirect('recipes')->with('message', 'You need to select recipes first !');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        //...
+        return redirect('recipes')->with('error', "There is an error, no entry deleted");
+    }
+}
