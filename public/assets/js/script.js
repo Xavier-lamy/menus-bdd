9e9967576bdbd5c8e4c6b8f89409a3c4eb118254
Xavier-lamy <xavier.lamy.pro@gmail.com>
2022-02-01 19:24:54 +0100

    Fix: not being able to delete row in modify recipe

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -4,28 +4,42 @@ var __webpack_exports__ = {};
   !*** ./resources/js/script.js ***!
   \********************************/
 //Add new ingredient row
-var addIngredientButton = document.getElementById('addIngredientButton');
-var ingredientOptions = document.getElementById('selectIngredientOptions').innerHTML;
-var incrementedId = document.getElementsByName('ingredientRow').length + 1;
-addIngredientButton.addEventListener('click', function () {
-  var buttonRow = document.getElementById('addIngredientRow');
-  var newRow = '<tr>' + '<td class="text--center p--1">' + '<select name="ingredient[' + incrementedId + '][command_id]" id="ingredient_id_' + incrementedId + '" aria-label="Ingredient (unit)" form="add_recipe_form" class="text--center input--inset" title="Ingredient (unit)" required>' + ingredientOptions + '</select>' + '</td>' + '<td>' + '<input type="number" aria-label="Quantity" min="0" name="ingredient[' + incrementedId + '][quantity]" form="add_recipe_form" class="text--center input--inset" placeholder="Quantity" required>' + '</td>' + '<td class="text--center p--1">' + '<button type="button" name="deleteRow" id="deleteRow' + incrementedId + '" class="button--sm">Delete row</button>' + '</td>' + '</tr>';
-  incrementedId++;
-  buttonRow.insertAdjacentHTML('beforebegin', newRow);
-}); //Delete an ingredient row
+var modifyRecipeTable = document.getElementById('modifyRecipeTable');
+var addRecipeTable = document.getElementById('addRecipeTable');
 
-window.onload = checkClickedButton();
+if (modifyRecipeTable || addRecipeTable) {
+  var checkClickedButton = function checkClickedButton() {
+    if (document.getElementById("modifyRecipeTable")) {
+      document.getElementById("modifyRecipeTable").onclick = clickedButton;
+    } else {
+      document.getElementById("addRecipeTable").onclick = clickedButton;
+    }
+  };
 
-function checkClickedButton() {
-  document.getElementById("addRecipeTable").onclick = clickedButton;
-}
+  var clickedButton = function clickedButton(e) {
+    if (e.target.name == 'deleteRow') {
+      targetId = e.target.id;
+      var elementToDelete = document.getElementById(targetId);
+      elementToDelete.closest('tr').remove();
+    }
+  };
 
-function clickedButton(e) {
-  if (e.target.name == 'deleteRow') {
-    targetId = e.target.id;
-    var elementToDelete = document.getElementById(targetId);
-    elementToDelete.closest('tr').remove();
-  }
+  var addIngredientButton = document.getElementById('addIngredientButton');
+  var ingredientOptions = document.getElementById('selectIngredientOptions').innerHTML;
+  var incrementedId = document.getElementsByName('ingredientRow').length + 1;
+  addIngredientButton.addEventListener('click', function () {
+    var buttonRow = document.getElementById('addIngredientRow');
+    var newRow = '<tr>' + '<td class="text--center p--1">' + '<select name="ingredient[' + incrementedId + '][command_id]" id="ingredient_id_' + incrementedId + '" aria-label="Ingredient (unit)" form="add_recipe_form" class="text--center input--inset" title="Ingredient (unit)" required>' + ingredientOptions + '</select>' + '</td>' + '<td>' + '<input type="number" aria-label="Quantity" min="0" name="ingredient[' + incrementedId + '][quantity]" form="add_recipe_form" class="text--center input--inset" placeholder="Quantity" required>' + '</td>' + '<td class="text--center p--1">' + '<button type="button" name="deleteRow" id="deleteRow' + incrementedId + '" class="button--sm">Delete row</button>' + '</td>' + '</tr>';
+
+    if (modifyRecipeTable) {
+      newRow = '<tr>' + '<td class="text--center p--1">' + '<select name="ingredient[' + incrementedId + '][command_id]" id="ingredient_id_' + incrementedId + '" aria-label="Ingredient (unit)" form="update_recipe_form" class="text--center input--inset" title="Ingredient (unit)" required>' + ingredientOptions + '</select>' + '</td>' + '<td>' + '<input type="number" aria-label="Quantity" min="0" name="ingredient[' + incrementedId + '][quantity]" form="update_recipe_form" class="text--center input--inset" placeholder="Quantity" required>' + '</td>' + '<td class="text--center p--1">' + '<button type="button" name="deleteRow" id="deleteRow' + incrementedId + '" class="button--sm">Delete row</button>' + '</td>' + '</tr>';
+    }
+
+    incrementedId++;
+    buttonRow.insertAdjacentHTML('beforebegin', newRow);
+  }); //Delete an ingredient row
+
+  window.onload = checkClickedButton();
 }
 /******/ })()
 ;
--- resources/js/script.js
+++ resources/js/script.js
@@ -1,41 +1,68 @@
 //Add new ingredient row
-let addIngredientButton = document.getElementById('addIngredientButton');
-const ingredientOptions = document.getElementById('selectIngredientOptions').innerHTML;
-let incrementedId = document.getElementsByName('ingredientRow').length + 1;
+let modifyRecipeTable = document.getElementById('modifyRecipeTable');
+let addRecipeTable = document.getElementById('addRecipeTable');
 
-addIngredientButton.addEventListener('click', function(){
-    let buttonRow = document.getElementById('addIngredientRow');
+if( modifyRecipeTable || addRecipeTable){
+  let addIngredientButton = document.getElementById('addIngredientButton');
+  const ingredientOptions = document.getElementById('selectIngredientOptions').innerHTML;
+  let incrementedId = document.getElementsByName('ingredientRow').length + 1;
 
-    let newRow = '<tr>'
-    + '<td class="text--center p--1">'
-    + '<select name="ingredient['+ incrementedId +'][command_id]" id="ingredient_id_'+ incrementedId +'" aria-label="Ingredient (unit)" form="add_recipe_form" class="text--center input--inset" title="Ingredient (unit)" required>'
-    + ingredientOptions
-    + '</select>'
-    + '</td>'
-    + '<td>'
-    + '<input type="number" aria-label="Quantity" min="0" name="ingredient['+ incrementedId +'][quantity]" form="add_recipe_form" class="text--center input--inset" placeholder="Quantity" required>'
-    + '</td>'
-    + '<td class="text--center p--1">'
-    + '<button type="button" name="deleteRow" id="deleteRow'+ incrementedId +'" class="button--sm">Delete row</button>'
-    + '</td>'
-    + '</tr>';
+  addIngredientButton.addEventListener('click', function(){
+      let buttonRow = document.getElementById('addIngredientRow');
 
-    incrementedId ++;
+      let newRow = '<tr>'
+      + '<td class="text--center p--1">'
+      + '<select name="ingredient['+ incrementedId +'][command_id]" id="ingredient_id_'+ incrementedId +'" aria-label="Ingredient (unit)" form="add_recipe_form" class="text--center input--inset" title="Ingredient (unit)" required>'
+      + ingredientOptions
+      + '</select>'
+      + '</td>'
+      + '<td>'
+      + '<input type="number" aria-label="Quantity" min="0" name="ingredient['+ incrementedId +'][quantity]" form="add_recipe_form" class="text--center input--inset" placeholder="Quantity" required>'
+      + '</td>'
+      + '<td class="text--center p--1">'
+      + '<button type="button" name="deleteRow" id="deleteRow'+ incrementedId +'" class="button--sm">Delete row</button>'
+      + '</td>'
+      + '</tr>';
 
-    buttonRow.insertAdjacentHTML('beforebegin', newRow);
-});
+      if(modifyRecipeTable){
+        newRow = '<tr>'
+        + '<td class="text--center p--1">'
+        + '<select name="ingredient['+ incrementedId +'][command_id]" id="ingredient_id_'+ incrementedId +'" aria-label="Ingredient (unit)" form="update_recipe_form" class="text--center input--inset" title="Ingredient (unit)" required>'
+        + ingredientOptions
+        + '</select>'
+        + '</td>'
+        + '<td>'
+        + '<input type="number" aria-label="Quantity" min="0" name="ingredient['+ incrementedId +'][quantity]" form="update_recipe_form" class="text--center input--inset" placeholder="Quantity" required>'
+        + '</td>'
+        + '<td class="text--center p--1">'
+        + '<button type="button" name="deleteRow" id="deleteRow'+ incrementedId +'" class="button--sm">Delete row</button>'
+        + '</td>'
+        + '</tr>';
+      }
 
-//Delete an ingredient row
-window.onload = checkClickedButton();
+      incrementedId ++;
 
-function checkClickedButton() {
-  document.getElementById("addRecipeTable").onclick = clickedButton;
-}
+      buttonRow.insertAdjacentHTML('beforebegin', newRow);
+  });
 
-function clickedButton(e) {
-  if (e.target.name == 'deleteRow') {
-    targetId = e.target.id;
-    let elementToDelete = document.getElementById(targetId);
-    elementToDelete.closest('tr').remove();
+  //Delete an ingredient row
+  window.onload = checkClickedButton();
+
+  function checkClickedButton() {
+    if(document.getElementById("modifyRecipeTable")){
+      document.getElementById("modifyRecipeTable").onclick = clickedButton;    
+    }
+    else {
+      document.getElementById("addRecipeTable").onclick = clickedButton;
+    }
   }
+
+  function clickedButton(e) {
+    if (e.target.name == 'deleteRow') {
+      targetId = e.target.id;
+      let elementToDelete = document.getElementById(targetId);
+      elementToDelete.closest('tr').remove();
+    }
+  }
+  
 }
