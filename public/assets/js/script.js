eaec8452dc58ecbcd2548c3e97a32b8fb56ffe84
Xavier-lamy <xavier.lamy.pro@gmail.com>
2022-02-03 22:08:43 +0100

    Add functions for adding and removing rows in menu

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -40,6 +40,74 @@ if (modifyRecipeTable || addRecipeTable) {
   }); //Delete an ingredient row
 
   window.onload = checkClickedButton();
+} //Add recipe row
+
+
+var modifyMenuTable = document.getElementById('modifyMenuTable');
+var addMenuTable = document.getElementById('addMenuTable');
+
+if (modifyMenuTable || addMenuTable) {
+  var _checkClickedButton = function _checkClickedButton() {
+    if (document.getElementById("modifyMenuTable")) {
+      document.getElementById("modifyMenuTable").onclick = _clickedButton;
+    } else {
+      document.getElementById("addMenuTable").onclick = _clickedButton;
+    }
+  };
+
+  var _clickedButton = function _clickedButton(e) {
+    if (e.target.name == 'deleteRow') {
+      targetId = e.target.id;
+      var elementToDelete = document.getElementById(targetId);
+      elementToDelete.closest('tr').remove();
+    }
+  };
+
+  var recipeOptions = document.getElementById('selectRecipeOptions').innerHTML; //Morning
+
+  var addMorningRecipeButton = document.getElementById('addMorningRecipeButton');
+  var incrementedMorningId = document.getElementsByName('recipeMorningRow').length + 1;
+  addMorningRecipeButton.addEventListener('click', function () {
+    var buttonRow = document.getElementById('addMorningRecipeRow');
+    var newRow = '<tr>' + '<td class="text--center p--1">' + '<select name="morning[' + incrementedMorningId + '][recipe]" id="recipe_morning_' + incrementedMorningId + '" aria-label="Recipe" form="add_menu_form" class="text--center input--inset" title="Recipe" required>' + recipeOptions + '</select>' + '</td>' + '<td>' + '<input type="number" aria-label="Portions" min="0" name="morning[' + incrementedMorningId + '][quantity]" form="add_menu_form" class="text--center input--inset" placeholder="Portions" required>' + '</td>' + '<td class="text--center p--1">' + '<button type="button" name="deleteRow" id="deleteMorningRow' + incrementedMorningId + '" class="button--sm">Delete row</button>' + '</td>' + '</tr>';
+
+    if (modifyRecipeTable) {
+      newRow = '<tr>' + '<td class="text--center p--1">' + '<select name="morning[' + incrementedMorningId + '][recipe]" id="recipe_morning_' + incrementedMorningId + '" aria-label="Recipe" form="update_menu_form" class="text--center input--inset" title="Recipe" required>' + recipeOptions + '</select>' + '</td>' + '<td>' + '<input type="number" aria-label="Portions" min="0" name="morning[' + incrementedMorningId + '][quantity]" form="update_menu_form" class="text--center input--inset" placeholder="Portions" required>' + '</td>' + '<td class="text--center p--1">' + '<button type="button" name="deleteRow" id="deleteMorningRow' + incrementedMorningId + '" class="button--sm">Delete row</button>' + '</td>' + '</tr>';
+    }
+
+    incrementedMorningId++;
+    buttonRow.insertAdjacentHTML('beforebegin', newRow);
+  }); //Noon
+
+  var addNoonRecipeButton = document.getElementById('addNoonRecipeButton');
+  var incrementedNoonId = document.getElementsByName('recipeNoonRow').length + 1;
+  addNoonRecipeButton.addEventListener('click', function () {
+    var buttonRow = document.getElementById('addNoonRecipeRow');
+    var newRow = '<tr>' + '<td class="text--center p--1">' + '<select name="noon[' + incrementedNoonId + '][recipe]" id="recipe_noon_' + incrementedNoonId + '" aria-label="Recipe" form="add_menu_form" class="text--center input--inset" title="Recipe" required>' + recipeOptions + '</select>' + '</td>' + '<td>' + '<input type="number" aria-label="Portions" min="0" name="noon[' + incrementedNoonId + '][quantity]" form="add_menu_form" class="text--center input--inset" placeholder="Portions" required>' + '</td>' + '<td class="text--center p--1">' + '<button type="button" name="deleteRow" id="deleteNoonRow' + incrementedNoonId + '" class="button--sm">Delete row</button>' + '</td>' + '</tr>';
+
+    if (modifyRecipeTable) {
+      newRow = '<tr>' + '<td class="text--center p--1">' + '<select name="noon[' + incrementedNoonId + '][recipe]" id="recipe_noon_' + incrementedNoonId + '" aria-label="Recipe" form="update_menu_form" class="text--center input--inset" title="Recipe" required>' + recipeOptions + '</select>' + '</td>' + '<td>' + '<input type="number" aria-label="Portions" min="0" name="noon[' + incrementedNoonId + '][quantity]" form="update_menu_form" class="text--center input--inset" placeholder="Portions" required>' + '</td>' + '<td class="text--center p--1">' + '<button type="button" name="deleteRow" id="deleteNoonRow' + incrementedNoonId + '" class="button--sm">Delete row</button>' + '</td>' + '</tr>';
+    }
+
+    incrementedNoonId++;
+    buttonRow.insertAdjacentHTML('beforebegin', newRow);
+  }); //Evening
+
+  var addEveningRecipeButton = document.getElementById('addEveningRecipeButton');
+  var incrementedEveningId = document.getElementsByName('recipeEveningRow').length + 1;
+  addEveningRecipeButton.addEventListener('click', function () {
+    var buttonRow = document.getElementById('addEveningRecipeRow');
+    var newRow = '<tr>' + '<td class="text--center p--1">' + '<select name="evening[' + incrementedEveningId + '][recipe]" id="recipe_evening_' + incrementedEveningId + '" aria-label="Recipe" form="add_menu_form" class="text--center input--inset" title="Recipe" required>' + recipeOptions + '</select>' + '</td>' + '<td>' + '<input type="number" aria-label="Portions" min="0" name="evening[' + incrementedEveningId + '][quantity]" form="add_menu_form" class="text--center input--inset" placeholder="Portions" required>' + '</td>' + '<td class="text--center p--1">' + '<button type="button" name="deleteRow" id="deleteEveningRow' + incrementedEveningId + '" class="button--sm">Delete row</button>' + '</td>' + '</tr>';
+
+    if (modifyRecipeTable) {
+      newRow = '<tr>' + '<td class="text--center p--1">' + '<select name="evening[' + incrementedEveningId + '][recipe]" id="recipe_evening_' + incrementedEveningId + '" aria-label="Recipe" form="update_menu_form" class="text--center input--inset" title="Recipe" required>' + recipeOptions + '</select>' + '</td>' + '<td>' + '<input type="number" aria-label="Portions" min="0" name="evening[' + incrementedEveningId + '][quantity]" form="update_menu_form" class="text--center input--inset" placeholder="Portions" required>' + '</td>' + '<td class="text--center p--1">' + '<button type="button" name="deleteRow" id="deleteEveningRow' + incrementedEveningId + '" class="button--sm">Delete row</button>' + '</td>' + '</tr>';
+    }
+
+    incrementedEveningId++;
+    buttonRow.insertAdjacentHTML('beforebegin', newRow);
+  }); //Delete a recipe row
+
+  window.onload = _checkClickedButton();
 }
 /******/ })()
 ;
--- resources/js/script.js
+++ resources/js/script.js
@@ -64,5 +64,158 @@ if( modifyRecipeTable || addRecipeTable){
       elementToDelete.closest('tr').remove();
     }
   }
-  
+}
+
+//Add recipe row
+let modifyMenuTable = document.getElementById('modifyMenuTable');
+let addMenuTable = document.getElementById('addMenuTable');
+
+if( modifyMenuTable || addMenuTable){
+  const recipeOptions = document.getElementById('selectRecipeOptions').innerHTML;
+
+  //Morning
+  let addMorningRecipeButton = document.getElementById('addMorningRecipeButton');
+  let incrementedMorningId = document.getElementsByName('recipeMorningRow').length + 1;
+
+  addMorningRecipeButton.addEventListener('click', function(){
+      let buttonRow = document.getElementById('addMorningRecipeRow');
+
+      let newRow = '<tr>'
+      + '<td class="text--center p--1">'
+      + '<select name="morning['+ incrementedMorningId +'][recipe]" id="recipe_morning_'+ incrementedMorningId +'" aria-label="Recipe" form="add_menu_form" class="text--center input--inset" title="Recipe" required>'
+      + recipeOptions
+      + '</select>'
+      + '</td>'
+      + '<td>'
+      + '<input type="number" aria-label="Portions" min="0" name="morning['+ incrementedMorningId +'][quantity]" form="add_menu_form" class="text--center input--inset" placeholder="Portions" required>'
+      + '</td>'
+      + '<td class="text--center p--1">'
+      + '<button type="button" name="deleteRow" id="deleteMorningRow'+ incrementedMorningId +'" class="button--sm">Delete row</button>'
+      + '</td>'
+      + '</tr>';
+
+      if(modifyRecipeTable){
+        newRow = '<tr>'
+        + '<td class="text--center p--1">'
+        + '<select name="morning['+ incrementedMorningId +'][recipe]" id="recipe_morning_'+ incrementedMorningId +'" aria-label="Recipe" form="update_menu_form" class="text--center input--inset" title="Recipe" required>'
+        + recipeOptions
+        + '</select>'
+        + '</td>'
+        + '<td>'
+        + '<input type="number" aria-label="Portions" min="0" name="morning['+ incrementedMorningId +'][quantity]" form="update_menu_form" class="text--center input--inset" placeholder="Portions" required>'
+        + '</td>'
+        + '<td class="text--center p--1">'
+        + '<button type="button" name="deleteRow" id="deleteMorningRow'+ incrementedMorningId +'" class="button--sm">Delete row</button>'
+        + '</td>'
+        + '</tr>';
+      }
+
+      incrementedMorningId ++;
+
+      buttonRow.insertAdjacentHTML('beforebegin', newRow);
+  });
+
+  //Noon
+  let addNoonRecipeButton = document.getElementById('addNoonRecipeButton');
+  let incrementedNoonId = document.getElementsByName('recipeNoonRow').length + 1;
+
+  addNoonRecipeButton.addEventListener('click', function(){
+      let buttonRow = document.getElementById('addNoonRecipeRow');
+
+      let newRow = '<tr>'
+      + '<td class="text--center p--1">'
+      + '<select name="noon['+ incrementedNoonId +'][recipe]" id="recipe_noon_'+ incrementedNoonId +'" aria-label="Recipe" form="add_menu_form" class="text--center input--inset" title="Recipe" required>'
+      + recipeOptions
+      + '</select>'
+      + '</td>'
+      + '<td>'
+      + '<input type="number" aria-label="Portions" min="0" name="noon['+ incrementedNoonId +'][quantity]" form="add_menu_form" class="text--center input--inset" placeholder="Portions" required>'
+      + '</td>'
+      + '<td class="text--center p--1">'
+      + '<button type="button" name="deleteRow" id="deleteNoonRow'+ incrementedNoonId +'" class="button--sm">Delete row</button>'
+      + '</td>'
+      + '</tr>';
+
+      if(modifyRecipeTable){
+        newRow = '<tr>'
+        + '<td class="text--center p--1">'
+        + '<select name="noon['+ incrementedNoonId +'][recipe]" id="recipe_noon_'+ incrementedNoonId +'" aria-label="Recipe" form="update_menu_form" class="text--center input--inset" title="Recipe" required>'
+        + recipeOptions
+        + '</select>'
+        + '</td>'
+        + '<td>'
+        + '<input type="number" aria-label="Portions" min="0" name="noon['+ incrementedNoonId +'][quantity]" form="update_menu_form" class="text--center input--inset" placeholder="Portions" required>'
+        + '</td>'
+        + '<td class="text--center p--1">'
+        + '<button type="button" name="deleteRow" id="deleteNoonRow'+ incrementedNoonId +'" class="button--sm">Delete row</button>'
+        + '</td>'
+        + '</tr>';
+      }
+
+      incrementedNoonId ++;
+
+      buttonRow.insertAdjacentHTML('beforebegin', newRow);
+  });
+
+  //Evening
+  let addEveningRecipeButton = document.getElementById('addEveningRecipeButton');
+  let incrementedEveningId = document.getElementsByName('recipeEveningRow').length + 1;
+
+  addEveningRecipeButton.addEventListener('click', function(){
+      let buttonRow = document.getElementById('addEveningRecipeRow');
+
+      let newRow = '<tr>'
+      + '<td class="text--center p--1">'
+      + '<select name="evening['+ incrementedEveningId +'][recipe]" id="recipe_evening_'+ incrementedEveningId +'" aria-label="Recipe" form="add_menu_form" class="text--center input--inset" title="Recipe" required>'
+      + recipeOptions
+      + '</select>'
+      + '</td>'
+      + '<td>'
+      + '<input type="number" aria-label="Portions" min="0" name="evening['+ incrementedEveningId +'][quantity]" form="add_menu_form" class="text--center input--inset" placeholder="Portions" required>'
+      + '</td>'
+      + '<td class="text--center p--1">'
+      + '<button type="button" name="deleteRow" id="deleteEveningRow'+ incrementedEveningId +'" class="button--sm">Delete row</button>'
+      + '</td>'
+      + '</tr>';
+
+      if(modifyRecipeTable){
+        newRow = '<tr>'
+        + '<td class="text--center p--1">'
+        + '<select name="evening['+ incrementedEveningId +'][recipe]" id="recipe_evening_'+ incrementedEveningId +'" aria-label="Recipe" form="update_menu_form" class="text--center input--inset" title="Recipe" required>'
+        + recipeOptions
+        + '</select>'
+        + '</td>'
+        + '<td>'
+        + '<input type="number" aria-label="Portions" min="0" name="evening['+ incrementedEveningId +'][quantity]" form="update_menu_form" class="text--center input--inset" placeholder="Portions" required>'
+        + '</td>'
+        + '<td class="text--center p--1">'
+        + '<button type="button" name="deleteRow" id="deleteEveningRow'+ incrementedEveningId +'" class="button--sm">Delete row</button>'
+        + '</td>'
+        + '</tr>';
+      }
+
+      incrementedEveningId ++;
+
+      buttonRow.insertAdjacentHTML('beforebegin', newRow);
+  });
+
+  //Delete a recipe row
+  window.onload = checkClickedButton();
+
+  function checkClickedButton() {
+    if(document.getElementById("modifyMenuTable")){
+      document.getElementById("modifyMenuTable").onclick = clickedButton;    
+    }
+    else {
+      document.getElementById("addMenuTable").onclick = clickedButton;
+    }
+  }
+
+  function clickedButton(e) {
+    if (e.target.name == 'deleteRow') {
+      targetId = e.target.id;
+      let elementToDelete = document.getElementById(targetId);
+      elementToDelete.closest('tr').remove();
+    }
+  }
 }
