6293752dfe0ff8d5b090bd775c8c26a8dbd6c593
Xavier-lamy <xavier.lamy.pro@gmail.com>
2022-03-03 20:16:46 +0100

    Update stock tests and controller with user_id

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -6,16 +6,28 @@ use Illuminate\Http\Request;
 use App\Models\Command;
 use App\Models\Stock;
 use App\Rules\CommandIdValided;
+use Illuminate\Support\Facades\Auth;
 
 class StockController extends Controller
 {
+    /**
+     * Define middlewares in class construct
+     */
+    public function __construct() {
+        //Force use of authentication
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index() {
-        $products = Stock::all();
+    public function index() 
+    {
+        $user_id = Auth::user()->id;
+
+        $products = Stock::where('user_id', $user_id)->get();
         
         return view('stocks', [
             'products' => $products,
@@ -30,9 +42,14 @@ class StockController extends Controller
      */
     public function create()
     {
+        $user_id = Auth::user()->id;
+
         $is_creating = true;
-        $products = Stock::all();
-        $commands_products = Command::orderBy('ingredient')->get();
+        $products = Stock::where('user_id', $user_id)->get();
+
+        $commands_products = Command::where('user_id', $user_id)
+            ->orderBy('ingredient')
+            ->get();
 
         return view('stocks', [
             'products' => $products,
@@ -49,6 +66,8 @@ class StockController extends Controller
      */
     public function store(Request $request)
     {
+        $user_id = Auth::user()->id;
+
         $request->validate([
             'quantity' => ['required', 'min:0', 'max:1000000000', 'integer'],
             'useby_date' => ['required', 'date_format:Y-m-d', 'after:2000-01-01', 'before:2300-01-01'],
@@ -56,12 +75,16 @@ class StockController extends Controller
         ]);
 
         $id = $request->command_id;
-        $commands_product = Command::find($id);
+        $commands_product = Command::where([
+            'id' => $id,
+            'user_id' => $user_id,
+        ])->first();
 
         Stock::create([
             'quantity' => $request->quantity,
             'useby_date' => $request->useby_date,
             'command_id' => $id,
+            'user_id' => $user_id,
         ]);
 
         $alert_stock = $commands_product->alert_stock;
@@ -83,14 +106,6 @@ class StockController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    /* public function show($id) */
-
-    /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
@@ -98,7 +113,9 @@ class StockController extends Controller
      */
     public function edit($id)
     {
-        $products = Stock::all();
+        $user_id = Auth::user()->id;
+
+        $products = Stock::where('user_id', $user_id)->get();
 
         return view('stocks', [
             'products' => $products,
@@ -114,6 +131,8 @@ class StockController extends Controller
      */
     public function update(Request $request)
     {
+        $user_id = Auth::user()->id;
+
         $request->validate([
             'id' => ['required', 'min:0', 'integer'],
             'command_id' => ['required', 'min:0', 'integer'],
@@ -128,7 +147,11 @@ class StockController extends Controller
         $useby_date = $request->useby_date;
         
         //Fetch ancient value for stock quantity
-        $stocks_product = Stock::find($stock_id);
+        $stocks_product = Stock::where([
+            'id' => $stock_id,
+            'user_id'=> $user_id,
+        ])->first();
+
         $old_stock_quantity = $stocks_product->quantity;
 
         //update product in stocks
@@ -138,7 +161,10 @@ class StockController extends Controller
         ]);
 
         //update product in commands
-        $commands_product = Command::find($command_id);
+        $commands_product = Command::where([
+            'id' => $command_id,
+            'user_id'=> $user_id,
+        ])->first();
 
         $alert_stock = $commands_product->alert_stock;
         $command_quantity = $commands_product->quantity;
@@ -165,10 +191,12 @@ class StockController extends Controller
      */
     public function confirmDestroy(Request $request)
     {
+        $user_id = Auth::user()->id;
+
         $delete_confirmation = 'stocks';
         $delete_ids = $request->except('_token');
 
-        $products = Stock::whereIn('id', $delete_ids)->get();
+        $products = Stock::where('user_id', $user_id)->whereIn('id', $delete_ids)->get();
 
         if($products->count() > 0){
             return view('confirmation', [
@@ -189,6 +217,8 @@ class StockController extends Controller
      */
     public function destroy(Request $request)
     {
+        $user_id = Auth::user()->id;
+
         $delete_ids = $request->except('_token');
         $entries_deleted = 0;
         $entries_total = count($delete_ids);
@@ -198,14 +228,21 @@ class StockController extends Controller
         //remove quantity from total stocks and modify must_buy
 
             //Stocks variables:
-            $stocks_product = Stock::find($id);
+            $stocks_product = Stock::where([
+                'id' => $id,
+                'user_id'=> $user_id,
+            ])->first();
 
             if(!empty($stocks_product)){
                 $command_id = $stocks_product->command_id;
                 $stock_quantity = $stocks_product->quantity;
 
                 //Commands variables:
-                $commands_product = Command::find($command_id);
+                $commands_product = Command::where([
+                    'id' => $command_id,
+                    'user_id'=> $user_id,
+                ])->first();
+                
                 $command_quantity = $commands_product->quantity;
                 $alert_stock = $commands_product->alert_stock;
                 $newquantity = $command_quantity - $stock_quantity;
--- tests/Feature/StockTest.php
+++ tests/Feature/StockTest.php
@@ -3,14 +3,73 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Database\Seeders\TestSeeders\TestDatabaseSeeder;
+use Tests\TestCase;
 use App\Models\Command;
 use App\Models\Stock;
-use Illuminate\Foundation\Testing\WithFaker;
-use Tests\TestCase;
+use App\Models\User;
 
 class StockTest extends TestCase
 {
     use RefreshDatabase;
+    /**
+     * Prepare the database for tests with the test seeders
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        
+        $this->seed(TestDatabaseSeeder::class);
+
+        $this->user = User::find(TestDatabaseSeeder::TESTENV_USER_ID);
+
+    }
+
+    /**
+     * Test only authenticated user can access to stocks view
+     * 
+     * @test
+     * @return void
+     */
+    public function stocksRouteRedirectToGuestIfNotAuth()
+    {
+        $response = $this->get('/stocks');
+
+        $response->assertRedirect('/guest');
+        $response->assertStatus(302);
+    }
+
+    /**
+     * Test stock index method and view
+     * 
+     * @test
+     * @return void
+     */
+    public function stockIndexIfAuth()
+    {
+        $response = $this->actingAs($this->user)->get('/stocks');
+
+        $response->assertViewIs('stocks')->assertStatus(200);
+    }
+
+    /**
+     * Test stock create method and view 
+     *
+     * @test
+     * @return void
+     */
+    public function stockCreateIfAuth()
+    {
+        $response = $this->actingAs($this->user)->get('/stocks/create');
+
+        $response
+            ->assertViewIs('stocks')
+            ->assertViewHas('is_creating')
+            ->assertViewHas('products')
+            ->assertViewHas('commands_products')
+            ->assertStatus(200);
+    }
+
     /**
      * Test if Stocks method "Store" work as intended:
      *  - Redirect
@@ -18,34 +77,22 @@ class StockTest extends TestCase
      *  - The product is correctly put in stocks
      *  - The product is correctly updated in commands
      *
+     * @test
      * @return void
      */
-    public function testStoreInStock()
+    public function stockStoreIfAuth()
     {
-        $this->withoutExceptionHandling();
-
-        //Create a product in command for test purpose
-        Command::create([
-            'id' => 1,
-            'ingredient' => 'flour',
-            'quantity' => 0,
-            'unit' => 'grams',
-            'alert_stock' => 150,
-            'must_buy' => 1,
-        ]);
-
-        //Test a request for adding product in stocks
-        $response = $this->post('/stocks/create', [
-            'quantity' => 200,
-            'useby_date' => '2002-11-01',
+        $response = $this->actingAs($this->user)->post('/stocks/create', [
+            'quantity' => 5,
+            'useby_date' => '2021-12-30',
             'command_id' => 1,
         ]);
 
         //Check if the product we just add is added correctly in database
         $stock = Stock::where([
-            'id' => 1,
-            'quantity' => 200,
-            'useby_date' => '2002-11-01',
+            'id' => 5, //Because there is already 4 in test stock
+            'quantity' => 5,
+            'useby_date' => '2021-12-30',
             'command_id' => 1,
         ])->exists();
         
@@ -54,11 +101,9 @@ class StockTest extends TestCase
         //Check if the product related to it in commands has been updated
         $command = Command::where([
             'id' => 1,
-            'ingredient' => 'flour',
-            'quantity' => 200,
-            'unit' => 'grams',
-            'alert_stock' => 150,
-            'must_buy' => 0,
+            'quantity' => 25, //Because there is already two stocks products with quantity = 10 for this product
+            'alert_stock' => 20,
+            'must_buy' => 0, //must_buy passed to false because now quantity > alert_stock
         ])->exists();
 
         $this->assertTrue($command);
@@ -71,47 +116,48 @@ class StockTest extends TestCase
     }
 
     /**
+     * Test stock edit method and view 
+     *
+     * @test
+     * @return void
+     */
+    public function stockEditIfAuth()
+    {
+        $response = $this->actingAs($this->user)->get('/stocks/modify/{id}');
+
+        $response
+            ->assertViewIs('stocks')
+            ->assertViewHas('products')
+            ->assertViewHas('modifying_product_id')
+            ->assertStatus(200);
+    }
+
+    /**
      * Test if Stocks method "Update" work as intended:
      *  - Redirect
      *  - Return a success message
      *  - The product is correctly updated in stocks
      *  - The product is correctly updated in commands
      *
+     * @test
      * @return void
      */
-    public function testUpdateStock()
+    public function stockUpdateIfAuth()
     {
-        $this->withoutExceptionHandling();
-
-        Command::create([
-            'id' => 1,
-            'ingredient' => 'flour',
-            'quantity' => 200,
-            'unit' => 'grams',
-            'alert_stock' => 150,
-            'must_buy' => 1,
-        ]);
-
-        Stock::create([
-            'id' => 1,
-            'quantity' => 200,
-            'useby_date' => '2002-11-01',
-            'command_id' => 1,
-        ]);
-
-        $response = $this->post('/stocks/modify', [
+        $response = $this->actingAs($this->user)->post('/stocks/modify', [
             'id' => 1,
             'command_id' => 1,
-            'useby_date' => '2002-11-01',
-            'quantity' => 600,
+            'useby_date' => '2021-12-30',
+            'quantity' => 5,
         ]);
 
         //Check if the product we just update has been updated correctly
         $stock = Stock::where([
             'id' => 1,
-            'quantity' => 600,
-            'useby_date' => '2002-11-01',
+            'quantity' => 5,
+            'useby_date' => '2021-12-30',
             'command_id' => 1,
+            'user_id' => $this->user->id,
         ])->exists();
         
         $this->assertTrue($stock);
@@ -119,11 +165,9 @@ class StockTest extends TestCase
         //Check if the product related to it in commands has been updated
         $command = Command::where([
             'id' => 1,
-            'ingredient' => 'flour',
-            'quantity' => 600,
-            'unit' => 'grams',
-            'alert_stock' => 150,
-            'must_buy' => 0,
+            'quantity' => 15,
+            'must_buy' => 1,
+            'user_id' => $this->user->id,
         ])->exists();
 
         $this->assertTrue($command);
@@ -133,6 +177,23 @@ class StockTest extends TestCase
             ->assertSessionHas([
                 'success' => 'Product modified !',
             ]);
+    }
+
+    /**
+     * Test stock delete-confirmation method and view 
+     *
+     * @test
+     * @return void
+     */
+    public function stockDeleteConfirmIfAuth()
+    {
+        $response = $this->actingAs($this->user)->post('/stock-delete-confirmation');
+
+        //Should return an error message and redirect to stocks, because no products are selected
+        $response
+            ->assertRedirect('/stocks')
+            ->assertSessionHas('message', 'You need to select products first !')
+            ->assertStatus(302);
     }
 
     /**
@@ -147,33 +208,8 @@ class StockTest extends TestCase
      */
     public function testDestroyStock()
     {
-        $this->withoutExceptionHandling();
-
-        Command::create([
-            'id' => 1,
-            'ingredient' => 'flour',
-            'quantity' => 500,
-            'unit' => 'grams',
-            'alert_stock' => 150,
-            'must_buy' => 0,
-        ]);
-
-        //Create 2 products in stocks
-        Stock::create([
-            'id' => 1,
-            'quantity' => 200,
-            'useby_date' => '2002-11-01',
-            'command_id' => 1,
-        ]);
-
-        Stock::create([
-            'id' => 2,
-            'quantity' => 300,
-            'useby_date' => '2002-11-03',
-            'command_id' => 1,
-        ]);
-
-        $response = $this->post('/stocks/delete', [
+        //Test destroying products 1 and 2 in stocks
+        $response = $this->actingAs($this->user)->post('/stocks/delete', [
             'delete_1' => 1,
             'delete_2' => 2,
         ]);
@@ -187,11 +223,10 @@ class StockTest extends TestCase
         //Check if the product related to it in commands was updated
         $command = Command::where([
             'id' => 1,
-            'ingredient' => 'flour',
             'quantity' => 0,
-            'unit' => 'grams',
-            'alert_stock' => 150,
+            'alert_stock' => 20,
             'must_buy' => 1,
+            'user_id' => $this->user->id,
         ])->exists();
 
         $this->assertTrue($command);
